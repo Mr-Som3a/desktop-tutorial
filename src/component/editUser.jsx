103b1bf52bdbfca999b73793dab823da82783f66
Musab Obada Y. Elsamani <musab0124887085@gmail.com>
2023-02-01 19:06:42 +0200

    updated

--- src/component/editUser.jsx
+++ src/component/editUser.jsx
@@ -9,11 +9,11 @@ class editUser extends Component {
           <h2>Edit User Information</h2>
           <form className="col g-3" id="editUser" onSubmit={(e) => this.props.onUpdate(e)}>
             {/* <Input type="hidden" name="id" label="Id" value={this.props.user._id} /> */}
-            <Input type="text" name="id" label="Id" value={this.props.user.id} onChange={(e) => this.props.onChange(e)} />
-            <Input type="text" name="name" label="Name" value={this.props.user.name} onChange={(e) => this.props.onChange(e)} />
-            <Input type="text" name="age" label="Age" value={this.props.user.age} onChange={(e) => this.props.onChange(e)} />
-            <Input type="text" name="address" label="Address" value={this.props.user.address} onChange={(e) => this.props.onChange(e)} />
-            <Input type="text" name="phone" label="Phone" value={this.props.user.phone} onChange={(e) => this.props.onChange(e)} />
+            <Input type="text" name="id" label="Id" value={this.props.user.id} onChange={this.props.onChange} />
+            <Input type="text" name="name" label="Name" value={this.props.user.name} onChange={this.props.onChange} />
+            <Input type="text" name="age" label="Age" value={this.props.user.age} onChange={this.props.onChange} />
+            <Input type="text" name="address" label="Address" value={this.props.user.address} onChange={this.props.onChange} />
+            <Input type="text" name="phone" label="Phone" value={this.props.user.phone} onChange={this.props.onChange} />
             <div className="col mt-2">
               <button className="btn btn-success m-1" type="submit">
                 Update
